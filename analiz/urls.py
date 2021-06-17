1a82ce32be50de3c4d42a63f1f891b83b73d399c
Sergei <Sergei7496844@mail.ru>
2021-06-17 15:10:23 +0300

    Correct filtering by date of brands

--- analiz/urls.py
+++ analiz/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
 	path('manager/<int:manager_id>/<str:brandname>', views.brand, name = "brand"),
 	path('product/<str:name>', views.productinfo , name = "product"),
 	path('regist', views.registerPage , name = "regist"),
-	path('manager/<int:manager_id>/<int:year>', views.filter_by_date, name = "filter_by_date"),
 ]
--- analiz/views.py
+++ analiz/views.py
@@ -78,20 +78,41 @@ def brand(request, manager_id, brandname=0):
 	managers = Manager.objects.all()
 	type = TypeOfProduct.objects.all().filter(brand=brandname)
 	product = Product.objects.all().filter(brand=brandname)
-	monthes = Product.objects.values('product_name', 'sales', 'date').annotate(month=TruncMonth('date')).annotate(sales_by_month=Sum('sales'))
-
 	
-	total_sales=[]
+
+	date1 = '2020-01-01'
+	date2 = datetime.now()
+	form = DateForm(request.POST or None)
+	if form.is_valid():
+		date1 = form.clean_date1()
+		date2 = form.clean_date2()
+		if date1==None or date2==None:
+			date1 = '2020-01-01'
+			date2 = datetime.now()
+		product = product.filter(date__gte=date1).filter(date__lte=date2)
+	if product.count() == 0:
+		product = Product.objects.all()
+	
+	total_sales = []
+	ave_sales = []
 	for item in type:
 		name = item.type
 		type_sales = 0
+		average_sales = 0
+		coun = 0
 		for p in product:
 			if name == p.product_name:
+				coun += 1
 				s = p.sales
 				type_sales += s
+				average_sales += s
+		if coun == 0:
+			coun = 1
+		average_sales = average_sales / coun
+		ave_sales.append(average_sales)
 		total_sales.append(type_sales)
 	
-	return render(request, 'MyPage.html', {'product' : product, 'managers' : managers, 'brand' : brand, 'type' : type, 'monthes': monthes, 'type_1':total_sales, 'brands':brands, 'manage_id' : manager_id})
+	return render(request, 'MyPage.html', {'form':form, 'product' : product, 'managers' : managers, 'type' : type, 'type_1':total_sales,'ave_sales':ave_sales , 'brands':brands, 'manage_id' : manager_id})
 
 
 
@@ -159,30 +180,3 @@ def productinfo(request, name):
 	
 	context= {'form' : form, 'date1': date1, 'date2': date2, 'products':products, 'sales':sales, 'bsr':bsr, 'rating':rating, 'name':name, 'data':data, 'asin':asin, 'link':link, 'event':event}
 	return render(request, 'Product.html', context)
-
-
-def filter_by_date(request, yaer=None, minth=None):
-	managers = Manager.objects.all()
-	type = TypeOfProduct.objects.all().filter(manager=manager_id)
-	brands = Brand.objects.all() 
-	type = TypeOfProduct.objects.all().filter(manager=manager_id)
-	
-	product = Product.objects.all().filter(manager=manager_id)
-	if minth != None:
-		product = product.annotate(month=TruncMonth('date')).filter(month=minth)
-	elif yaer != None:
-		product = product.annotate(year=TruncMonth('date')).filter(year=yaer)
-	else:
-		pass
-	
-	total_sales=[]
-	for item in type:
-		name = item.type
-		type_sales = 0
-		for p in product:
-			if name == p.product_name:
-				s = p.sales
-				type_sales += s
-		total_sales.append(type_sales)
-		
-	return render(request, 'MyPage.html', {'product' : product, 'managers' : managers, 'type' : type, 'type_1':total_sales, 'brands':brands, 'manage_id' : manager_id})
