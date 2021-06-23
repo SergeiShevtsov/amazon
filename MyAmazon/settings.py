4f16232777e1ac9a55f26e9d251d2ccff12f1d83
Sergei <Sergei7496844@mail.ru>
2021-06-23 15:24:41 +0300

    Add search field

--- MyAmazon/settings.py
+++ MyAmazon/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'analiz',
-    'import_export',
 ]
 
 MIDDLEWARE = [
--- analiz/admin.py
+++ analiz/admin.py
@@ -2,10 +2,10 @@ from django.contrib import admin
 from .models import Product, Manager, Brand, TypeOfProduct
 from django.contrib.auth.models import User
 from django.utils.translation import gettext, gettext_lazy as _
-from import_export import resources
-from import_export.admin import ImportExportModelAdmin
-from import_export import fields
-from import_export.widgets import ForeignKeyWidget
+# from import_export import resources
+# from import_export.admin import ImportExportModelAdmin
+# from import_export import fields
+# from import_export.widgets import ForeignKeyWidget
 
 admin.site.register(Brand)
 
