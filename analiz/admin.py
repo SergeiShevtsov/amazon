066878796c9e8c9d2a06c0488e8a21ae7ae2347b
Sergei <Sergei7496844@mail.ru>
2021-06-24 16:13:48 +0300

    H

--- analiz/admin.py
+++ analiz/admin.py
@@ -2,38 +2,17 @@ from django.contrib import admin
 from .models import Product, Manager, Brand, TypeOfProduct
 from django.contrib.auth.models import User
 from django.utils.translation import gettext, gettext_lazy as _
-from import_export import resources
-from import_export.admin import ImportExportModelAdmin
-from import_export import fields
-from import_export.widgets import ForeignKeyWidget
+# from import_export import resources
+# from import_export.admin import ImportExportModelAdmin
+# from import_export import fields
+# from import_export.widgets import ForeignKeyWidget
 
+admin.site.register(Brand)
 
-
-class ProductResource(resources.ModelResource):
-	brand = fields.Field(column_name='brand', attribute='brand', widget=ForeignKeyWidget(Brand,'brand'))
-	manager=fields.Field(column_name='manager', attribute='manager', widget=ForeignKeyWidget(Manager,'username'))
-	type = fields.Field(column_name='type', attribute='type', widget=ForeignKeyWidget(TypeOfProduct,'type'))
-	
-	# category = fields.Field(column_name='category', attribute='category', widget=ForeignKeyWidget(ProductCategory,'name'))
-	class Meta:
-		model = Product
-	
-
-class ProductAdmin(ImportExportModelAdmin):
-	resources_class = ProductResource
-	list_display=[field.name for field in Product._meta.fields if field.name != 'id']
-
-admin.site.register(Product, ProductAdmin)
-
-@admin.register(Brand)
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-	list_display = ('id', 'brand')
-
-
-# @admin.register(Product)
-# class ProductAdmin(admin.ModelAdmin):
-# 	list_display = ("product_name", "id", "date", 'reviews', 'sales', 'price', 'bsr', 'brand', 'manager', 'link')
-# 	search_fields = ('product_name',)
+	list_display = ("product_name", "id", "date", 'reviews', 'sales', 'price', 'bsr', 'brand', 'manager', 'link')
+	search_fields = ('product_name',)
 
 @admin.register(TypeOfProduct)
 class ProductAdmin(admin.ModelAdmin):
